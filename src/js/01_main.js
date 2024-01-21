65a02e304f4abdf07e5ec2f2cc9b45ab1e438fd0
12345 <a12345@192.168.0.14>
2024-01-21 14:13:06 +0700

    готово

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -21,3 +21,61 @@ var swiper = new Swiper('.slider', {
 		el: '.slider-pagination',
 	},
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+	const modal = document.getElementById('modal')
+	const btn = document.querySelector('.btn.catalog-btn')
+	const closeBtn = document.querySelector('.close')
+
+	btn.addEventListener('click', function () {
+		modal.style.display = 'block'
+	})
+
+	closeBtn.addEventListener('click', function () {
+		modal.style.display = 'none'
+	})
+
+	window.addEventListener('click', function (event) {
+		if (event.target === modal) {
+			modal.style.display = 'none'
+		}
+	})
+})
+
+fetch('data.json')
+	.then(response => response.json())
+	.then(data => {
+		// Создание элементов меню на основе данных
+		const menuContainer = document.getElementById('menuContainer')
+		const rightInnerContainers = document.querySelectorAll(
+			'.modal__right-inner'
+		)
+
+		data.forEach((item, index) => {
+			const menuItem = document.createElement('a')
+			menuItem.href = '#'
+			menuItem.classList.add('modal__main-left-list')
+
+			menuItem.innerHTML = `
+									<div class="modal__main-left-list__left">${item.label}</div>
+									<div class="modal__main-left-list__right">
+											<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+													<path d="M6.66671 3.33337L13.3334 10L6.66671 16.6667" stroke="#91A5B7" stroke-width="2" stroke-linejoin="round" />
+											</svg>
+									</div>
+							`
+
+			menuItem.addEventListener('mouseover', () => {
+				// Убираем класс active у всех контейнеров modal__right-inner
+				rightInnerContainers.forEach(container => {
+					container.classList.remove('active')
+				})
+
+				// Добавляем класс active к соответствующему контейнеру modal__right-inner
+				rightInnerContainers[index].classList.add('active')
+			})
+
+			menuContainer.appendChild(menuItem)
+		})
+	})
+	.catch(error => console.error('Ошибка загрузки данных:', error))
